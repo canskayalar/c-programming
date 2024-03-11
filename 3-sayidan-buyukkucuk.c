bbc9da5ce8eec24353b13fdadc8fec68b8ffe55e
canskayalar <cansukayalar5@gmail.com>
2024-03-11 13:20:08 +0300

    first

---
+++ 3-sayidan-buyukkucuk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int maxmin(int array[],int uzunluk){
+    int max=array[0];
+    int min=array[0];
+    for(int i=1; i<uzunluk; i++){
+    if(array[i]>max){
+        max=array[i];
+    }if(array[i]<min){
+        min=array[i];
+    }
+    }
+    printf("max : %d\n" , max);
+    printf("min : %d" , min);
+}
+int main(){
+    int array[5]={4,6,9,2,1};
+    int uzunluk=sizeof(array)/sizeof(array[0]);
+    maxmin(array,uzunluk);
+}
